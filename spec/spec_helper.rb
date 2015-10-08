d7226d694e7cb231645ce58fa4eb201ade26669b
Colin Surprenant <colin.surprenant@gmail.com>
2015-10-08 17:17:58 -0400

    remove usage of RSpec.configure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require "logstash/devutils/rspec/spec_helper"
 
 # this has been taken from the udp input, it should be DRYed
 
-module TcpHelpers
+class TcpHelpers
 
   def pipelineless_input(plugin, size, &block)
     queue = Queue.new
@@ -12,16 +12,11 @@ module TcpHelpers
     end
     block.call
     sleep 0.1 while queue.size != size
-    result = nevents.times.inject([]) do |acc|
+    result = size.times.inject([]) do |acc|
       acc << queue.pop
     end
     plugin.do_stop
     input_thread.join
     result
   end
-
-end
-
-RSpec.configure do |c|
-  c.include TcpHelpers
 end
