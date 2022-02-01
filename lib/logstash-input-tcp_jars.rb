038acbe088f205cf25f1a0c577292f8c3ffe46dc
Karol Bucek <kares@users.noreply.github.com>
2022-02-01 15:54:20 +0100

    Build: skip shadowing jar dependencies (#187)

--- lib/logstash-input-tcp_jars.rb
+++
@@ -1,4 +0,0 @@
-# encoding: utf-8
-
-require 'jar_dependencies'
-require_jar('org.logstash.inputs', 'logstash-input-tcp', ::File.read(::File.join(::File.dirname(__FILE__), '../version')).split("\n").first)
