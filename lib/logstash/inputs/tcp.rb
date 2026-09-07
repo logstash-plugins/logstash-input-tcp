8d426bf99595ea6f05c680a67892a16099566559
Andrea Selva <selva.andre@gmail.com>
2026-09-07 11:05:03 +0200

    Aggregate HA proxy line before parsing it (#286)
    Avoids error that could rise if the TCP stack decides to break the HA Proxy v1 headline before the newline terminator in multiple byte arrays.
    
    Adds an aggregator to collect all the bytes up to the first newline before move on to the decoding part done by DecoderImpl class. This message decoder is added only if proxy setting is enabled.
    
    Co-authored-by: Cas Donoghue <cas.donoghue@gmail.com>

--- lib/logstash/inputs/tcp.rb
+++ lib/logstash/inputs/tcp.rb
@@ -181,7 +181,7 @@ class LogStash::Inputs::Tcp < LogStash::Inputs::Base
     validate_ssl_config!
 
     if server?
-      @loop = InputLoop.new(@id, @host, @port, DecoderImpl.new(@codec, self), @tcp_keep_alive, java_ssl_context)
+      @loop = InputLoop.new(@id, @host, @port, DecoderImpl.new(@codec, self), @tcp_keep_alive, java_ssl_context, @proxy_protocol)
     end
   end
 
